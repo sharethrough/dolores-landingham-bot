51d2ed0951c5b9bd7a17ed70fed1a7dc02291031
Carlo Costino <carlo.costino@gsa.gov>
2015-10-08 15:57:18 -0400

    Connects #33:  Improved the description of a couple of the EmployeeFinder specs.

--- spec/services/employee_finder_spec.rb
+++ spec/services/employee_finder_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe EmployeeFinder do
   describe "#employee_exists?" do
-    it "returns true if an employee exists in Slack" do
+    it "returns true if an employee exists in the Slack organization associated with the auth token" do
       employee = create(:employee)
       client_double = Slack::Web::Client.new
       existing_user_double = double(existing_user?: true)
@@ -17,7 +17,7 @@ describe EmployeeFinder do
       expect(employee_finder).to be_existing_employee
     end
 
-    it "returns false if an employee does not exist in Slack" do
+    it "rreturns false if an employee does not exist in the Slack organization associated with the auth token" do
       employee = create(:employee)
       client_double = Slack::Web::Client.new
       existing_user_double = double(existing_user?: false)
