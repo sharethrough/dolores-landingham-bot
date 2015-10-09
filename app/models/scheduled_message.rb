8f9ece143cabeed5e03203d958850b018ec8ceb5
Carlo Costino <carlo.costino@gsa.gov>
2015-10-09 10:11:11 -0400

    Connects #28:  Improved tag handling
    This changeset adds a few improvements to the tag handling such as being able to enter them separated by spaces instead of commas, better form configuration with labels and hints, spelling corrections, and improved tests.

--- app/models/scheduled_message.rb
+++ app/models/scheduled_message.rb
@@ -1,6 +1,7 @@
 class ScheduledMessage < ActiveRecord::Base
   validates :body, presence: true
   validates :days_after_start, presence: true
+  validates :tag_list, presence: true
   validates :title, presence: true
 
   acts_as_taggable
--- spec/features/edit_scheduled_messages_spec.rb
+++ spec/features/edit_scheduled_messages_spec.rb
@@ -1,32 +1,17 @@
 require "rails_helper"
 
 feature "Edit scheduled messages" do
-  scenario "from list of scheduled messages without tags" do
+  scenario "successfully" do
     old_title = "Old title"
     new_title = "New title"
+    tags = "tag_one tag_two tag_three"
     create(:scheduled_message, title: old_title)
 
     login_with_oauth
     visit scheduled_messages_path
     click_on "Edit"
     fill_in "Title", with: new_title
-    click_on "Update Scheduled message"
-
-    expect(page).to have_content "Scheduled message updated successfully"
-    expect(page).to have_content new_title
-  end
-
-  scenario "from list of scheduled messages with tags" do
-    old_title = "Old title"
-    new_title = "New title"
-    tags = "tag_one, tag_two, tag_three"
-    create(:scheduled_message, title: old_title)
-
-    login_with_oauth
-    visit scheduled_messages_path
-    click_on "Edit"
-    fill_in "Title", with: new_title
-    fill_in "Tags (separated by commas)", with: tags
+    fill_in "Tags", with: tags
     click_on "Update Scheduled message"
 
     expect(page).to have_content "Scheduled message updated successfully"
@@ -34,5 +19,22 @@ feature "Edit scheduled messages" do
     expect(page).to have_content "tag_one"
     expect(page).to have_content "tag_two"
     expect(page).to have_content "tag_three"
+  end
+
+  scenario "unsuccessfully due to missing required fields" do
+    old_title = "Old title"
+    new_title = "New title"
+    tags = ""
+    create(:scheduled_message, title: old_title)
+
+    login_with_oauth
+    visit scheduled_messages_path
+    click_on "Edit"
+    fill_in "Title", with: new_title
+    fill_in "Tags", with: tags
+    click_on "Update Scheduled message"
+
+    expect(page).to have_content "Could not update scheduled message"
+    expect(page).to have_content "can't be blank"
   end
 end
